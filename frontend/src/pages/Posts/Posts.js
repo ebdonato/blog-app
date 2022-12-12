9ee18b98e6d5a3fc4e336fa358f8a7a33d043c78
Eduardo Donato <eduardo.donato@gmail.com>
2022-12-12 01:34:03 -0300

    query Posts

--- frontend/src/pages/Posts/Posts.js
+++ frontend/src/pages/Posts/Posts.js
@@ -1,6 +1,35 @@
-import React from "react";
-import Post from "../../components/Post/Post";
+import React from "react"
+import Post from "../../components/Post/Post"
+import {gql, useQuery} from "@apollo/client"
+
+const GET_POST = gql`
+    query Posts {
+        posts {
+            id
+            title
+            content
+            createdAt
+            user {
+                name
+            }
+        }
+    }
+`
 
 export default function Posts() {
-  return <div></div>;
+    const {data, error, loading} = useQuery(GET_POST)
+
+    if (error) return <div>Error Page</div>
+
+    if (loading) return <div>Loading...</div>
+
+    const {posts} = data
+
+    return (
+        <div>
+            {posts.map(({id, title, content, createdAt: date, user: {name: user}}) => (
+                <Post {...{title, content, date, user, id}} key={id} />
+            ))}
+        </div>
+    )
 }
