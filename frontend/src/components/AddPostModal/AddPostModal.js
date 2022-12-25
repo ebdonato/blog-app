a6af38db2a61d714d1d6445bb0c7efa6b76d8933
Eduardo Donato <eduardo.donato@gmail.com>
2022-12-24 22:51:24 -0300

    add add post

--- frontend/src/components/AddPostModal/AddPostModal.js
+++ frontend/src/components/AddPostModal/AddPostModal.js
@@ -1,67 +1,94 @@
-import React, { useState } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import React, {useEffect, useState} from "react"
+import {Modal, Button, Form} from "react-bootstrap"
+
+import {gql, useMutation} from "@apollo/client"
+
+const POST_CREATE = gql`
+    mutation PostCreate($content: String!, $title: String!) {
+        postCreate(post: {title: $title, content: $content}) {
+            userErrors {
+                message
+            }
+            post {
+                id
+                title
+            }
+        }
+    }
+`
 
 export default function AddPostModal() {
-  const [show, setShow] = useState(false);
+    const [postCreate, {data /*loading*/}] = useMutation(POST_CREATE)
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const [show, setShow] = useState(false)
+    const [error, setError] = useState(null)
 
-  const [content, setContent] = useState("");
-  const [title, setTitle] = useState("");
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
 
-  const handleClick = () => {};
+    const [content, setContent] = useState("")
+    const [title, setTitle] = useState("")
 
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Add Post
-      </Button>
+    const handleClick = () => {
+        postCreate({
+            variables: {
+                content,
+                title,
+            },
+        })
+    }
 
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Add Post</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder=""
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </Form.Group>
+    useEffect(() => {
+        if (data) {
+            data.postCreate.userErrors.length && setError(data.postCreate.userErrors[0].message)
 
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Content</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClick}>
-            Add
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+            data.postCreate.post && handleClose()
+        }
+    }, [data])
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Add Post
+            </Button>
+
+            <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Post</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder=""
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Content</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                value={content}
+                                onChange={(e) => setContent(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    {error && <p>{error}</p>}
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClick}>
+                        Add
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
 }
